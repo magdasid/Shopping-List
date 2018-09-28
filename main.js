264d029a6b7abedae0b39ddafd67043215963aea
magdasid <magda_si@wp.pl>
2018-09-28 22:44:41 +0200

    Change variables

--- main.js
+++ main.js
@@ -1,47 +1,46 @@
-let addButton = document.querySelector("#add-button");
-let productName = document.querySelector(".product-input");
-let amount = document.querySelector(".amount-input");
+const addButton = document.querySelector("#add-button");
+const productName = document.querySelector(".product-input");
+const amount = document.querySelector(".amount-input");
 
-let counter = (function(){
+const counter = (function(){
     let i = 0;
     return function() {
         i+=1;
         return i;
     };
 })();
-let shoppingList = (function(){
-    let items = [];
+const shoppingList = (function(){
+    const items = [];
     return function(...products){
         items.push(...products);
         return items;
     }
 })();
-let addProduct = (name, amount) => {
+const addProduct = (name, amount) => {
     let i = counter();
-    let element = document.createElement("div");
+    const element = document.createElement("div");
     element.innerHTML = `${name}, ilość: ${amount} <span class='delete${i}'><i class="far fa-trash-alt"></i></span>`;
     element.classList.add("product"+i,"product");
     document.getElementById("list-items").appendChild(element);
     document.getElementsByClassName('delete'+i)[0].addEventListener('click', () => deleteProduct(i, name));
-
-    let items = shoppingList(i, name, amount);
+    const items = shoppingList(i, name, amount);
     localStorage.setItem("array", JSON.stringify(items));
 }
-let deleteProduct = (id) => {
+const deleteProduct = (id) => {
     document.getElementsByClassName('product'+id)[0].remove();
-    let items = shoppingList();
-    let elementIndex = items.indexOf(id);
+    const items = shoppingList();
+    const elementIndex = items.indexOf(id);
     items.splice(elementIndex, 3);
     localStorage.setItem("array", JSON.stringify(items));
 }
 addButton.addEventListener('click', () => addProduct(productName.value, amount.value)); 
 document.getElementsByClassName("print-button")[0].addEventListener('click', () => window.print()); 
 
-let getProducts = (function(){
-    let products = JSON.parse(localStorage.getItem("array"));
+const getProducts = (function(){
+    const products = JSON.parse(localStorage.getItem("array"));
         for (let j=0; j< products.length; j=j+3) {
-            let name = products[j+1];
-            let amount = products[j+2];
+            const name = products[j+1];
+            const amount = products[j+2];
             addProduct(name,amount);
         }
 })();
